6e471f67f93710470194110fd4a27da006f26a79
HaroldMtz <144304582+HaroldMtz@users.noreply.github.com>
2025-10-19 11:30:53 -0600

    Primer commit - Actividad 3.3 Flutter Hello World

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Catálogo de Películas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
